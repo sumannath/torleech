db2b284f138c0ac9755cc87892dd85c5633c173b
Suman Nath <suman.pro@gmail.com>
2023-09-28 16:23:55 +0530

    Added drivers Path to firefox binary

--- config/constants.py
+++ config/constants.py
@@ -3,6 +3,7 @@ import os.path
 from config import credentials
 
 APP_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DRIVER_DIR = os.path.join(APP_DIR, "drivers")
 DRIVER_FILE_NAME = "geckodriver.exe"
 
 USERNAME = credentials.USERNAME
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os.path
+import platform
 
 from selenium.common import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,12 +13,34 @@ from selenium.webdriver.support.wait import WebDriverWait
 from config import constants
 
 
+def get_service_for_selenium_driver():
+    system_name = platform.system()
+    system_machine = platform.machine()
+
+    if system_machine == "Windows":
+        if system_machine == "x86_64":
+            service = Service(executable_path=os.path.join(constants.DRIVER_DIR, "win64", "geckodriver.exe"))
+        else:
+            service = Service(executable_path=os.path.join(constants.DRIVER_DIR, "win32", "geckodriver.exe"))
+    elif system_name == "Linux":
+        if system_machine == "x86_64":
+            service = Service(executable_path=os.path.join(constants.DRIVER_DIR, "linux64", "geckodriver"))
+        else:
+            service = Service(executable_path=os.path.join(constants.DRIVER_DIR, "linux32", "geckodriver"))
+    else:
+        service = None
+
+    return service
+
+
 def start_run():
     options = Options()
     options.headless = True
     options.add_argument("--headless")
 
-    service = Service(executable_path=os.path.join(constants.APP_DIR, "drivers", constants.DRIVER_FILE_NAME))
+    options.binary_location = os.path.join(constants.DRIVER_DIR, "firefox", "firefox")
+
+    service = get_service_for_selenium_driver()
 
     driver = webdriver.Firefox(options=options, service=service)
     driver.get("https://www.torrentleech.org/user/account/login/")
